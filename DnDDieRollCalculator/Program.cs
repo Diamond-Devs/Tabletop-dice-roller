3ee8764c9b5934e5ca201c5e5c007fcc914dcbad
Riley Lynch <rileydlynch@yahoo.com>
2022-10-24 11:35:56 -0400

    write calculateRoll() method

--- DnDDieRollCalculator/Program.cs
+++ DnDDieRollCalculator/Program.cs
@@ -1,2 +1,21 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System;
+namespace RollReturn
+{
+    class Program
+    {
+        static int CalculateRoll(int dievalue)
+        {
+
+            Random random = new Random();
+            int Roll = 0;
+
+            Roll += random.Next(1, dievalue);
+
+            return Roll;
+        }
+        
+        static void Main(string[] args)
+        {
+        }
+    }
+}
