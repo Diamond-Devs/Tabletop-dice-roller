dd0aeb33bf31c8e1b774c0f7b29f89df25a42408
Riley Lynch <rileydlynch@yahoo.com>
2022-10-25 21:55:00 -0400

    import discord api packages

--- DnDDieRollCalculator/Program.cs
+++ DnDDieRollCalculator/Program.cs
@@ -1,2 +1,8 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using Discord;
+using Discord.WebSocket;
+using System;
+using System.IO;
+using System.Threading.Tasks;
+
+// See https://aka.ms/new-console-template for more information
 Console.WriteLine("Hello, World!");
